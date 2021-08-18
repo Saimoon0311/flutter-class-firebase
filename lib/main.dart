466a2b360e4f8e7c4fbb818d90a4b69e9efb7ce8
Saimoon0311 <shaheer031123@gmail.com>
2021-08-18 06:13:36 -0700

    incomplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
+// import 'dart:html';
 import 'package:database/register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Register(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return Container();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(primarySwatch: Colors.blue),
+            home: Register(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(color: Colors.blue);
+      },
     );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -1,7 +1,10 @@
+// import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
@@ -10,35 +13,54 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    void register() async {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      final String username = usernameController.text;
+      final String email = emailController.text;
+      final String password = passwordController.text;
+      try {
+      final UserCredential user = await auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        firestore.collection("users").doc("uuu").set({
+          "email": email,
+          "username": username,
+        });
+        print("object" + username);
+      } catch (e) {
+        print("error");
+      }
+    }
+
     return Scaffold(
-      body:Container(
-        padding:const EdgeInsets.symmetric(horizontal: 10),
-        child: SafeArea(
-          child:Column(
-            children: [
-              TextFormField(
-  decoration: InputDecoration(
-    border: UnderlineInputBorder(),
-    labelText: 'Enter your username'
-  ),
-),
-            TextFormField(
-  decoration: InputDecoration(
-    border: UnderlineInputBorder(),
-    labelText: 'Enter your email'
-  ),
-),
-            TextFormField(
-  decoration: InputDecoration(
-    border: UnderlineInputBorder(),
-    labelText: 'Enter your password'
-  ),
-),
-ElevatedButton(onPressed: (){}, child: Text("data"))
-            ],
-          )
-        )
-      )
-    );
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: SafeArea(
+                child: Column(
+              children: [
+                TextFormField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your username'),
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your email'),
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your password'),
+                ),
+                ElevatedButton(onPressed: register, child: Text("data"))
+              ],
+            ))));
   }
 }
